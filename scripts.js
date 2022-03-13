29967f0112fa00800a19d9df74f2f2804a083935
Angelo Albuquerque <angelocarvalhot@gmail.com>
2022-03-13 20:50:43 -0300

    initial commit

---
+++ scripts.js
@@ -0,0 +1,46 @@
+//Arrays
+
+//let familia = [15,35,60,17];
+//console.log(familia.length);
+//console.log(familia[0]);
+
+//let unicap = [20,30,50,60,100];
+//console.log(unicap.length);
+//console.log(unicap[3]);
+
+//funções
+//verbo + substantivo
+//let corSite = "azul";
+//function resetaCor(){
+  // corSite = "";
+//};
+
+//console.log(corSite);
+//resetaCor();
+//console.log(corSite);
+
+
+//função que realiza uma tarefa e não devolve nada
+
+//function dizerNome(){
+  //console.log('');
+//}
+//dizerNome();
+
+//Faz um cálculo ou operação e retorna algo
+//function multiplicarPorDois(valor){
+  //  return valor* 2;
+//}
+
+//console.log(multiplicarPorDois(5));
+
+//Operadores aritméticos
+let salario = 100;
+
+console.log(salario + salario);
+console.log(salario - salario);
+console.log(salario * salario);
+console.log(salario / salario);
+
+let idade = 20;
+console.log(++idade);
